39413a6af65f60b87f9198fcf4f84172370098b4
jonny <532600519@qq.com>
2019-11-13 18:07:09 +0800

    checkout cloud eureka

--- servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
+++ servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
@@ -3,9 +3,12 @@ package com.changqin;
 import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
-@SpringBootApplication
-@EnableServiceComb
+@SpringCloudApplication
+//@EnableServiceComb
+@EnableFeignClients
 public class ServiceCombProvider {
 
     public static void main(String[] args) {
