08fc7cdbc4d846b4cb47c86f451a9967ea2b9842
jonny <532600519@qq.com>
2019-11-13 18:30:46 +0800

    checkout cloud eureka

--- servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
+++ servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
@@ -1,6 +1,5 @@
 package com.changqin;
 
-import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
