6c5e4c949f594d24bbe045651ac784fba983de5d
jonny <532600519@qq.com>
2019-11-14 15:45:56 +0800

    RSA认证.调用追踪

--- servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
+++ servicecomb-provider/src/main/java/com/changqin/ServiceCombProvider.java
@@ -1,11 +1,13 @@
 package com.changqin;
 
 import org.apache.servicecomb.springboot.starter.provider.EnableServiceComb;
+import org.apache.servicecomb.tracing.zipkin.EnableZipkinTracing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableServiceComb
+@EnableZipkinTracing
 public class ServiceCombProvider {
 
     public static void main(String[] args) {
--- servicecomb-provider/src/main/java/com/changqin/rpc/BusinessServiceImpl.java
+++ servicecomb-provider/src/main/java/com/changqin/rpc/BusinessServiceImpl.java
@@ -2,6 +2,7 @@ package com.changqin.rpc;
 
 import io.swagger.annotations.SwaggerDefinition;
 import org.apache.servicecomb.provider.pojo.RpcSchema;
+import org.apache.servicecomb.tracing.Span;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class BusinessServiceImpl implements BusinessService {
 
     @RequestMapping(value = "/test",method = RequestMethod.POST)
+    @Span
     public String say(@RequestParam(name="name") String name) {
         return "hello "+name;
     }
